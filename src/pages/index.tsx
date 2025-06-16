61b68346e79b56da9d023a3a2459641bd2dd3d21
ugurturkeli <ugurrturkeli@gmail.com>
2025-06-17 02:47:12 +0300

    Create index.tsx

---
+++ src/pages/index.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ padding: "50px", textAlign: "center" }}>
+      <h1>Vision Mobilya'ya Hoş Geldiniz</h1>
+      <p>Hayalinizdeki evi tasarlamak için buradayız.</p>
+    </div>
+  );
+}
